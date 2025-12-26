8e908efc980ab47cc16cb0876e84794a2dd1b41c
TJCur <jackcurrie2310@gmail.com>
2025-12-26 13:36:55 +0100

    frontend: refactors user components under users directory

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { AdminDashboardComponent } from './admin/dashboard/dashboard.component';
-import { OwnerDashboardComponent } from './restaurantOwner/dashboard/dashboard.component';
-import { CustomerDashboardComponent } from './customer/dashboard/dashboard.component';
+import { AdminDashboardComponent } from './users/admin/dashboard/dashboard.component';
+import { OwnerDashboardComponent } from './users/restaurantOwner/dashboard/dashboard.component';
+import { CustomerDashboardComponent } from './users/customer/dashboard/dashboard.component';
 import { AdminGuard } from './guards/admin.guard';
 import { OwnerGuard } from './guards/owner.guard';
 import { CustomerGuard } from './guards/customer.guard';
