145b7028f15ce83ebdb36dc1d83f91f5e7bfd735
TJCur <jackcurrie2310@gmail.com>
2025-12-28 10:32:05 +0100

    backend: adds get restaurants route

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -22,4 +22,24 @@ router.get('/users', async (req, res) => {
     }
 });
 
+router.get('/restaurants', async (req, res) => {
+    try {
+        const query = {
+            text: 'SELECT * FROM restaurant',
+            values: []
+        };
+
+        const results = await pool.query(query);
+
+        if (results.rows.length <= 0) {
+            return res.status(404).json({ error: "Nothing found" });
+        }
+
+        res.status(200).json(results.rows);
+    } catch (error) {
+        console.error("Error while fetching restaurants:", error.message);
+        res.status(500).json({ error: "Error while fetching restaurants: " + error.message });
+    }
+});
+
 module.exports = router;
