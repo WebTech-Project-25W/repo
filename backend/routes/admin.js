cc4bdc68a76d593b120250eb61b9c4b699334f7b
TJCur <jackcurrie2310@gmail.com>
2026-01-28 18:46:08 +0100

    backend: updates get admin/users for admins and owners

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -3,19 +3,55 @@ const router = express.Router();
 const pool = require('../pool.js');
 
 router.get('/users', async (req, res) => {
+  const { role, limit = 50, offset = 0 } = req.query;
+
   try {
+    // validate role input
+    const allowedRoles = ['customer', 'restaurantowner', 'sitemanager'];
+    const table = role.toLowerCase();
+
+    if (!allowedRoles.includes(table)) {
+      return res.status(400).json({ error: "Invalid role provided" });
+    }
+
     const query = {
-      text: 'SELECT * FROM AppUser ORDER BY email',
+      // template literal is risky, but the input is being screened before being accepeted
+      text: `SELECT u.email, u.firstname, u.lastname, COUNT(*) OVER() as totalEntries
+        FROM appuser u JOIN ${role} r on u.email = r.email
+        WHERE 1=1`,
       values: []
     };
 
-    const results = await pool.query(query);
-
-    if (results.rows.length <= 0) {
-      return res.status(404).json({ error: "Nothing found" });
+    const keys = ['email', 'firstName', 'lastName'];
+    // dynamic filters
+    for (const key of keys) {
+      const value = req.query[key];
+      if (value) {
+        query.values.push(`%${value}%`);
+        query.text += ` AND u.${key} ILIKE $${query.values.length}`;
+      }
     }
 
-    res.status(200).json(results.rows);
+    query.text += ` ORDER BY email DESC LIMIT $${query.values.length + 1} OFFSET $${query.values.length + 2}`;
+    query.values.push(limit, offset);
+
+
+    const results = await pool.query(query);
+
+    const totalEntries = results.rows.length > 0 ? parseInt(results.rows[0].totalentries) : 0;
+
+    res.status(200).json({
+      metadata: {
+        totalEntries,
+        limit: parseInt(limit),
+        offset: parseInt(offset),
+      },
+      data: results.rows.map(row => {
+        const { totalentries, ...data } = row;
+        return data;
+      })
+    })
+
   } catch (error) {
     console.error("Error while fetching users:", error.message);
     res.status(500).json({ error: "Error while fetching users: " + error.message });
@@ -23,9 +59,9 @@ router.get('/users', async (req, res) => {
 });
 
 router.get('/customers', async (req, res) => {
-  const { 
-    email, firstName, lastName, status, 
-    postcode, deliveryZone, limit = 50, offset = 0 
+  const {
+    email, firstName, lastName, status,
+    postcode, deliveryZone, limit = 50, offset = 0
   } = req.query;
 
   // JOIN AppUser and Customer
@@ -76,7 +112,7 @@ router.get('/customers', async (req, res) => {
     const results = await pool.query(query, params);
     const totalEntries = results.rows.length > 0 ? parseInt(results.rows[0].totalentries) : 0;
 
-    res.json({
+    res.status(200).json({
       metadata: {
         totalEntries,
         limit: parseInt(limit),
@@ -109,13 +145,13 @@ router.patch('/customers/:email/blocked-status', async (req, res) => {
     const results = await pool.query(query);
 
     if (results.rows.length <= 0) {
-      return res.status(409).json({ 
-        error: `Could not update status. Either the user "${customerEmail}" doesn't exist, or the transition from the current state to "${updatedBlockedStatus}" is not permitted.` 
+      return res.status(409).json({
+        error: `Could not update status. Either the user "${customerEmail}" doesn't exist, or the transition from the current state to "${updatedBlockedStatus}" is not permitted.`
       });
     }
 
     res.status(200).json(results.rows[0]);
-    
+
   } catch (error) {
     console.error(`Error while updating blocked status for: "${customerEmail}"`, error.message);
     res.status(500).json({ error: "Internal Server Error" });
