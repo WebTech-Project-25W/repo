6ebc27d1e20e0517a3ad4beaa1e97abbb4540cd3
TJCur <jackcurrie2310@gmail.com>
2026-01-31 13:25:12 +0100

    frontend: blocked login attempt message displayed

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,62 +1,61 @@
-  import { Component } from '@angular/core';
-  import { FormsModule } from '@angular/forms';
-  import { AuthService } from '../services/auth.service';
-  import { Router } from '@angular/router';
+import { Component, CSP_NONCE } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
-  @Component({
-    selector: 'app-login',
-    standalone: true,
-    imports: [FormsModule],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.css'
-  })
-  export class LoginComponent {
-    email = '';
-    password = '';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  email = '';
+  password = '';
 
-    constructor(
-      private authService: AuthService,
-      private router: Router
-    ) {}
-  
-    onLogin() {
-      this.authService.login(this.email, this.password).subscribe({
-        next: (response) => {
-          console.log('Login successful!', response);
-          localStorage.setItem('role', response.role); // Store the role
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
-          const role = response.role;
-          
-          if (role === "SiteManager") {
-            this.router.navigate(['/admin/dashboard']);
-          } else if (role === "RestaurantOwner") {
-            this.router.navigate(['/owner/dashboard']);
-          } else if (role === "Customer") {
-            this.router.navigate(['/customer/dashboard']);
-          }
+  onLogin() {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (response) => {
+        console.log('Login successful!', response);
+        localStorage.setItem('role', response.role); // Store the role
 
-        },
-       error: (err) => {
-  console.error('Login error full object:', err);
+        const role = response.role;
 
-  let message = 'Unable to login';
+        if (role === "SiteManager") {
+          this.router.navigate(['/admin/dashboard']);
+        } else if (role === "RestaurantOwner") {
+          this.router.navigate(['/owner/dashboard']);
+        } else if (role === "Customer") {
+          this.router.navigate(['/customer/dashboard']);
+        }
 
-  if (err?.error?.message) {
-    message = err.error.message;
-  } else if (typeof err?.error === 'string') {
-    message = err.error;
-  } else if (err?.status === 0) {
-    message = 'Cannot reach server (CORS or backend down)';
+      },
+      error: (err) => {
+        let message = 'Unable to login';
+        if (err?.error?.error) {
+          message = err.error.error;
+        } else if (err?.error?.message) {
+          message = err.error.message;
+        } else if (typeof err?.error === 'string') {
+          message = err.error;
+        } else if (err?.status === 0) {
+          message = 'Cannot reach server (CORS or backend down)';
+        }
+
+        alert('Login failed: ' + message);
+      }
+
+
+    });
   }
 
-  alert('Login failed: ' + message);
-}
-
-
-      });
-    }
-
-    toRegistration() {
-      this.router.navigate(['/auth/registration']);
-    }
-  } 
+  toRegistration() {
+    this.router.navigate(['/auth/registration']);
+  }
+} 
