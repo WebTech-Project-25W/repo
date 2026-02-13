48819735926048cdf228cb8d4d556d9a7e5d01d8
TJCur <jackcurrie2310@gmail.com>
2026-02-13 21:22:40 +0100

    frontend-admin: refactor admin layout to use shared app-side bar ui component

--- frontend/src/app/users/admin/admin-layout/admin-layout.component.ts
+++ frontend/src/app/users/admin/admin-layout/admin-layout.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SidebarComponent } from '../../../shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SidebarComponent],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css'
 })
 export class AdminLayoutComponent {
+  adminMenu = [
+    { label: 'Overview', route: '/admin/overview' },
+    { label: 'Order Log', route: '/admin/order-log' },
+    { label: 'Login Log', route: '/admin/login-log' },
+    { label: 'Users', route: '/admin/users' },
+    { label: 'Restaurants', route: '/admin/restaurants' },
+    { label: 'Global Settings', route: '/admin/global-settings' }
+  ]
 
 }
