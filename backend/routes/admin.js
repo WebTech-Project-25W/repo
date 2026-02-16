33d8e096b375011145cf51bcae97644fbb4381d2
TJCur <jackcurrie2310@gmail.com>
2026-02-16 21:11:47 +0100

    backend-admin-routes: reordes declaration and use of sub routers

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../pool.js');
 
 const userRoutes = require('./admin/users.js');
-router.use('/users', userRoutes);
-
 const restaurantRoutes = require('./admin/restaurants.js')
-router.use('/restaurants', restaurantRoutes);
-
 const logRoutes = require('./admin/logs.js');
-router.use('/logs', logRoutes);
-
 const analyticsRoutes = require('./admin/analytics.js');
-router.use('/analytics', analyticsRoutes);
-
 const voucherRoutes = require('./admin/vouchers.js');
-router.use('/vouchers', voucherRoutes);
-
 const deliveryZoneRoutes = require('./admin/deliveryZones.js')
-router.use('/delivery-zones', deliveryZoneRoutes);
-
 const profileRoutes = require('./admin/profile.js');
+
+router.use('/users', userRoutes);
+router.use('/restaurants', restaurantRoutes);
+router.use('/logs', logRoutes);
+router.use('/analytics', analyticsRoutes);
+router.use('/vouchers', voucherRoutes);
+router.use('/delivery-zones', deliveryZoneRoutes);
 router.use('/profile', profileRoutes);
 
 module.exports = router;
