b3f7acd38f42287054514c83e57adad408f94b20
TJCur <jackcurrie2310@gmail.com>
2026-02-16 22:40:19 +0100

    backend-admin: adds route to retrieve accumulative fees

--- backend/routes/admin/analytics.js
+++ backend/routes/admin/analytics.js
@@ -22,4 +22,40 @@ router.get('/key-stats', async (req, res) => {
   }
 });
 
+router.get('/accumulative-fees', async (req, res) => {
+  try {
+    const query = `
+      With DailyRevenue AS (
+	      SELECT DATE(oh.time) AS order_date,
+        SUM(
+          CASE
+            WHEN o.serviceFeeType = 'cents' THEN o.serviceFee / 100.0
+            WHEN o.serviceFeeType = 'percent' THEN (items.subtotal * (o.serviceFee / 100.0))
+            ELSE 0 
+          END
+        ) AS daily_sum
+        FROM orderHistory AS oh 
+        JOIN "Order" AS o ON oh.orderid = o.orderid
+        JOIN (
+          SELECT orderID, SUM(quantity * unitprice) AS subtotal FROM orderitem
+          GROUP BY orderID
+        ) items ON items.orderID = o.orderID
+        WHERE oh.status = 'delivered'
+        GROUP BY DATE(oh.time)
+      )
+      SELECT 
+        order_date,
+        daily_sum,
+        SUM(daily_sum) OVER (ORDER BY order_date)
+      FROM DailyRevenue
+      ORDER BY order_date
+    `;
+
+    const result = await pool.query(query);
+    res.status(200).json(result.rows);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+})
+
 module.exports = router;
